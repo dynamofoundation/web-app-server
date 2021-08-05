83b72d3e5dde6b279a06191f0a90b64e52d4fcc7
dynamo-foundation <81651363+dynamo-foundation@users.noreply.github.com>
2021-08-05 10:34:15 -0400

    implement webpack cache

--- web-app-server/Global.cs
+++ web-app-server/Global.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 
 namespace web_app_server
 {
@@ -14,6 +15,17 @@ namespace web_app_server
 
         public static uint randSeed;
         public static Object randLock = new Object();
+
+        public class WebPack
+        {
+            public string hash;
+            public string indexPage;
+            public Dictionary<string, byte[]> pages;
+        }
+
+        public static Dictionary<string, WebPack> webPacks = new Dictionary<string, WebPack>();
+
+
         public static uint RandomNum(uint x)
         {
             lock (randLock)
@@ -61,6 +73,83 @@ namespace web_app_server
         public static string FullNodePass()
         {
             return settings["FullNodePass"];
+        }
+
+
+        public static string WebPackReadString (byte[] data, ref int pointer)
+        {
+            string result = "";
+            int len = WebPackReadInt(data, ref pointer);
+            for (int i = 0; i < len; i++)
+                result += Convert.ToChar(data[pointer + i]);
+            pointer += len;
+            return result;
+        }
+
+        public static int WebPackReadInt(byte[] data, ref int pointer)
+        {
+            int result = 0;
+            result += data[pointer] << 24;
+            result += data[pointer+1] << 16;
+            result += data[pointer+2] << 8;
+            result += data[pointer+3];
+
+            pointer += 4;
+
+            return result;
+        }
+
+        public static byte[] WebPackReadBinary(byte[] data, ref int pointer)
+        {
+            int len = WebPackReadInt(data, ref pointer);
+            byte[] result = new byte[len];
+            for (int i = 0; i < len; i++)
+                result[i] = data[pointer + i];
+            pointer += len;
+            return result;
+        }
+
+        public static void LoadWebPack(string assetHash, byte[] compressedPack, string indexPage)
+        {
+
+            byte[] pack = Decompress(compressedPack);
+
+            WebPack p = new WebPack();
+            p.hash = assetHash;
+            p.indexPage = indexPage;
+            p.pages = new Dictionary<string, byte[]>();
+
+            int pointer = 0;
+            while (pointer < pack.Length)
+            {
+                
+                string pageName = WebPackReadString(pack, ref pointer);
+                byte[] data = WebPackReadBinary(pack, ref pointer);
+                p.pages.Add(pageName, data);
+            }
+            webPacks.Add(assetHash, p);
+        }
+
+        public static byte[] Decompress(byte[] data)
+        {
+            using (var compressedStream = new MemoryStream(data))
+            using (var zipStream = new GZipStream(compressedStream, CompressionMode.Decompress))
+            using (var resultStream = new MemoryStream())
+            {
+                zipStream.CopyTo(resultStream);
+                return resultStream.ToArray();
+            }
+        }
+
+        public static byte[] GetWebPackPage(string hash, string page)
+        {
+            byte[] result = new byte[0];
+
+            if (webPacks.ContainsKey(hash))
+                if (webPacks[hash].pages.ContainsKey(page))
+                    result = webPacks[hash].pages[page];
+
+            return result;
         }
 
     }
--- web-app-server/WebWorker.cs
+++ web-app-server/WebWorker.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Net;
 using System.Text;
 using System.Net.Http;
+using Newtonsoft.Json.Linq;
 
 namespace web_app_server
 {
@@ -14,6 +15,7 @@ namespace web_app_server
         public HttpListenerContext context;
         static readonly HttpClient client = new HttpClient();
 
+        static HttpWebRequest webRequest;
 
 
         public void run()
@@ -25,28 +27,82 @@ namespace web_app_server
                 StreamReader reader = new StreamReader(request.InputStream, request.ContentEncoding);
                 string text = reader.ReadToEnd();
 
-                /*
-                dynamic rpcData = JsonConvert.DeserializeObject<dynamic>(text);
-                string method = rpcData.method;
-                Global.UpdateRand((uint)text.Length);
-                */
-
                 string[] path = request.RawUrl.Substring(1).Split("/");
 
-                string strResponse = getNFT(path[0], path[1]);
+
+                //array to store the response in
+                byte[] binaryData;
+
+
+                //if the hash is a webpack and it's loaded, then respond with the page from cache
+                if (Global.webPacks.ContainsKey(path[0]))
+                {
+                    if (path.Length > 1)
+                    {
+                        string fullPath = "";
+                        for (int i = 1; i < path.Length - 1; i++)
+                            fullPath = fullPath + path[i] + "\\";
+                        fullPath = fullPath + path[path.Length - 1];
+                        binaryData = Global.GetWebPackPage(path[0], fullPath);
+                    }
+                    else
+                    {
+                        binaryData = Global.GetWebPackPage(path[0], Global.webPacks[path[0]].indexPage);
+                    }
+
+                }
+                else
+                {
+
+                    string strResponse = GetAsset(path[0]);
+
+                    byte[] binaryResponse = HexToByte(strResponse);
+
+                    string assetHash;
+                    string assetClassHash;
+                    string metaData;
+                    string owner;
+                    string txnID;
+
+                    int offset = 0;
+                    assetHash = ReadString(binaryResponse, ref offset);
+                    assetClassHash = ReadString(binaryResponse, ref offset);
+                    metaData = ReadString(binaryResponse, ref offset);
+                    binaryData = ReadVector(binaryResponse, ref offset);
+                    owner = ReadString(binaryResponse, ref offset);
+                    txnID = ReadString(binaryResponse, ref offset);
+
+                    bool webpack = false;
+                    try
+                    {
+                        dynamic jMeta = JsonConvert.DeserializeObject(metaData);
+                        if ((jMeta.webpack_version == 1) && (jMeta.index_file.ToString().Length > 0))
+                            webpack = true;
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+
+                    if (webpack)
+                    {
+                        dynamic jMeta = JsonConvert.DeserializeObject(metaData);
+                        Global.LoadWebPack(assetHash, binaryData, jMeta.index_file.ToString());
+                        binaryData = Global.GetWebPackPage(path[0], Global.webPacks[path[0]].indexPage);
+
+                    }
+                }
 
 
                 HttpListenerResponse response = context.Response;
 
-                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(strResponse);
-                response.ContentLength64 = buffer.Length;
                 System.IO.Stream output = response.OutputStream;
-                output.Write(buffer, 0, buffer.Length);
+                output.Write(binaryData, 0, binaryData.Length);
                 output.Close();
 
                 uint sum = 0;
-                for (int i = 0; i < buffer.Length; i++)
-                    sum += buffer[i];
+                for (int i = 0; i < binaryData.Length; i++)
+                    sum += binaryData[i];
 
                 Global.UpdateRand(sum);
             }
@@ -58,10 +114,108 @@ namespace web_app_server
             }
         }
 
-        public string getNFT(string assetClass, string asset)
+        public string ReadString ( byte[] data, ref int offset)
         {
-            return "hi";
+            string result = "";
+            int len = (data[offset] << 24) + (data[offset + 1] << 16) + (data[offset + 2] << 8) + (data[offset + 3]);
+            for (int i = 0; i < len; i++)
+                result += Convert.ToChar(data[offset + i + 4]);
+            offset += len + 4;
+            return result;
         }
+
+        public byte[] ReadVector(byte[] data, ref int offset)
+        {
+            byte[] result;
+            int len = (data[offset] << 24) + (data[offset + 1] << 16) + (data[offset + 2] << 8) + (data[offset + 3]);
+            result = new byte[len];
+            System.Array.Copy(data, offset + 4, result, 0, len);
+            offset += len + 4;
+            return result;
+        }
+
+
+        public static string GetAsset(string hash)
+        {
+            string result = "error";
+
+            string command = "get-asset";
+            string getcommand = "{ \"id\": 0, \"method\" : \"getnft\", \"params\" : [ \"" + command + "\", \"" + hash + "\" ] }";
+
+            try
+            {
+                string rpcResult = rpcExec(getcommand);
+                dynamic jRPCResult = JObject.Parse(rpcResult);
+                result = jRPCResult.result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+
+            return result;
+        }
+
+
+        public static string rpcExec(string command)
+        {
+            webRequest = (HttpWebRequest)WebRequest.Create(Global.FullNodeRPC());
+            webRequest.KeepAlive = false;
+            webRequest.Timeout = 300000;
+
+            var data = Encoding.ASCII.GetBytes(command);
+
+            webRequest.Method = "POST";
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+            webRequest.ContentLength = data.Length;
+
+            var username = Global.FullNodeUser();
+            var password = Global.FullNodePass();
+            string encoded = System.Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(username + ":" + password));
+            webRequest.Headers.Add("Authorization", "Basic " + encoded);
+
+
+            using (var stream = webRequest.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+
+            var webresponse = (HttpWebResponse)webRequest.GetResponse();
+
+            string submitResponse = new StreamReader(webresponse.GetResponseStream()).ReadToEnd();
+
+            webresponse.Dispose();
+
+
+            return submitResponse;
+        }
+
+
+        public byte[] HexToByte(string data)
+        {
+            data = data.ToUpper();
+            byte[] result = new byte[data.Length / 2];
+            for (int i = 0; i < data.Length; i += 2)
+            {
+                byte hi = hex(data[i]);
+                byte lo = hex(data[i + 1]);
+                result[i / 2] = (byte)(hi * 16 + lo);
+            }
+
+            return result;
+        }
+
+        public byte hex(char data)
+        {
+
+            if (data < 'A')
+                return (byte)(data - '0');
+            else
+                return (byte)((data - 'A') + 10);
+        }
+
 
     }
 }
