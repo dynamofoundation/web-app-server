f6c35b787de0bdf0a481ee35cc3dda2c4dac28ab
dynamo-foundation <81651363+dynamo-foundation@users.noreply.github.com>
2021-11-23 17:43:08 -0500

    update for prod

--- web-app-server/Log.cs
+++ web-app-server/Log.cs
@@ -9,7 +9,8 @@ namespace web_app_server
     {
         public static void log(string data)
         {
-            File.AppendAllText("log.txt", data + "\r\n");
+            Console.WriteLine(data);
+            //File.AppendAllText("log.txt", data + "\r\n");
         }
     }
 }
--- web-app-server/generate_wallet.js
+++ web-app-server/generate_wallet.js
@@ -2,6 +2,7 @@ var bitcoin = require('bitcoinjs-lib')
 var bip39 = require('bip39')
 var bip32 = require('bip32')
 
+var network = bitcoin.networks.bitcoin;
 
 const mnemonic = bip39.generateMnemonic();
 var masterSeed = bip39.mnemonicToSeedSync(mnemonic);
@@ -10,7 +11,6 @@ var xprv = node.toBase58();
 
 var seedWords = mnemonic.split(" ");
 
-var network = bitcoin.networks.bitcoin;
 var root = bip32.fromBase58(xprv, network);
 
 var child = root.derivePath("m/0'/0'/0'");
